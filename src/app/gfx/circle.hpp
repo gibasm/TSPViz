9cf98551e8f36bfcf8986e1caf20bd7aa9347499
TheMadMike <m.gibas2407@gmail.com>
2023-02-05 22:23:36 +0100

    Add Circle class

---
+++ src/app/gfx/circle.hpp
@@ -0,0 +1,34 @@
+#ifndef CIRCLE_HPP
+#define CIRCLE_HPP
+#include "drawable.hpp"
+#include <cmath>
+
+template <size_t N = 64> 
+class Circle :public Drawable {
+    SDL_FPoint points[N];
+    SDL_FPoint center;
+public:
+    constexpr Circle(float radius = 1.0f, SDL_FPoint center = {0.0f, 0.0f}) {
+        this->center = center;
+        float t = 0.0f;
+        float increase = (2.0f*M_PI) / static_cast<float>(N-1);
+        for(size_t i = 0; i < N; ++i) {
+            points[i].x = radius * sinf(t) + center.x;
+            points[i].y = radius * cosf(t) + center.y;
+            t += increase;
+        }
+    }
+
+    virtual ~Circle() {} 
+
+    SDL_FPoint get_center() {
+        return center;
+    }
+
+    virtual void draw(SDL_Renderer* renderer) override {
+        SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderDrawLinesF(renderer, points, N);
+    }
+};
+
+#endif /* CIRCLE_HPP */
